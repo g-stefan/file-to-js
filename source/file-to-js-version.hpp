d0e00f69aabea5f98ffeb93335b5de4dc641da36
Grigore Stefan <g_stefan@yahoo.com>
2021-07-06 20:19:46 +0300

    update

--- source/file-to-js-version.hpp
+++ source/file-to-js-version.hpp
@@ -10,10 +10,10 @@
 #ifndef FILE_TO_JS_VERSION_HPP
 #define FILE_TO_JS_VERSION_HPP
 
-#define FILE_TO_JS_VERSION_ABCD                1,5,0,7
-#define FILE_TO_JS_VERSION_STR                 "1.5.0"
-#define FILE_TO_JS_VERSION_STR_BUILD           "7"
-#define FILE_TO_JS_VERSION_STR_DATETIME        "2021-03-15 12:30:20"
+#define FILE_TO_JS_VERSION_ABCD                1,6,0,8
+#define FILE_TO_JS_VERSION_STR                 "1.6.0"
+#define FILE_TO_JS_VERSION_STR_BUILD           "8"
+#define FILE_TO_JS_VERSION_STR_DATETIME        "2021-07-06 15:11:44"
 
 #ifndef XYO_RC
 
